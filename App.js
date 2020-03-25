93ba8cb18572502332f52528ce786ecedfbb2f6d
rashinkafernando <rashinkaprameshi@gmail.com>
2020-03-25 19:07:35 +0530

    imported libraries

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector';
+import { SearchProvider, Results, SearchBox } from '@elastic/react-search-ui';
+import {Layout} from '@elastic/react-search-ui-views';
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
